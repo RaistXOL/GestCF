09be51388136e45483f7818a4d80cb04d6a4e4b6
Gian <Gian@SOGGIORNOPC>
2021-12-03 19:19:40 +0100

    Aggiungere i file di progetto.

---
+++ GestCF/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lbl_status = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lbl_status
+        '
+        Me.lbl_status.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_status.Location = New System.Drawing.Point(12, 205)
+        Me.lbl_status.Name = "lbl_status"
+        Me.lbl_status.Size = New System.Drawing.Size(365, 25)
+        Me.lbl_status.TabIndex = 0
+        Me.lbl_status.Text = "in attesa inserimento cns"
+        Me.lbl_status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.GestCF.My.Resources.Resources.image_processing20200819_32463_j2xi89
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(365, 190)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(389, 239)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lbl_status)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.TopMost = True
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lbl_status As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ GestCF/Form1.vb
@@ -0,0 +1,82 @@
+﻿Imports PCSC
+Imports PCSC.Monitoring
+
+Public Class Form1
+
+    Public oMonitorFactory
+    Public oMonitor As ISCardMonitor
+
+    Private Sub oMonitor_StatusChanged(sender As Object, e As StatusChangeEventArgs)
+        'MsgBox(e.NewState)
+
+    End Sub
+
+    Private Sub oMonitor_CardInserted(sender As Object, e As CardStatusEventArgs)
+        SetText(lbl_status, "card inserita")
+    End Sub
+
+    Private Sub oMonitor_CardRemoved(sender As Object, e As CardStatusEventArgs)
+
+        SetText(lbl_status, "card rimossa o in attesa carta")
+    End Sub
+
+    Private Sub SetText(ByVal ctl As Control, ByVal cText As String)
+        If ctl.InvokeRequired Then
+            ctl.BeginInvoke(New Action(Of Control, String)(AddressOf SetText), ctl, cText)
+        Else
+            ctl.Text = cText
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim oContextFactory = ContextFactory.Instance
+        Dim oReaders As String()
+
+        oMonitorFactory = MonitorFactory.Instance
+        oMonitor = oMonitorFactory.Create(SCardScope.System)
+
+        AddHandler oMonitor.StatusChanged, AddressOf oMonitor_StatusChanged
+        AddHandler oMonitor.CardInserted, AddressOf oMonitor_CardInserted
+        AddHandler oMonitor.CardRemoved, AddressOf oMonitor_CardRemoved
+
+
+        Using oContext As ISCardContext = oContextFactory.Establish(SCardScope.System)
+            oReaders = oContext.GetReaders
+
+            If oReaders.Length > 1 Then
+                For Each readername In oReaders
+                    lbl_status.Text = "inserire card nel lettore " & readername
+                Next
+            Else
+                lbl_status.Text = "inserire card nel lettore " & oReaders(0)
+            End If
+
+            oMonitor.Start(oReaders(0))
+            'oScCardMonitor.Start(oReaders(0))
+
+            'Dim oCardReader As ICardReader = oContext.ConnectReader(oReaders(0), SCardShareMode.Shared, SCardProtocol.Any)
+
+            'MsgBox(oCardReader.GetStatus.Protocol)
+
+
+
+            'oCardReader.Disconnect(SCardReaderDisposition.Reset)
+            'oCardReader.Dispose()
+
+            MsgBox(oMonitor.GetCurrentStateValue(0))
+
+
+
+
+        End Using
+
+
+
+
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        oMonitor.Cancel()
+        oMonitor.Dispose()
+    End Sub
+End Class
